99a7d88d87fc8d56e3a34a031f65a38344228637
denis <denis961111@gmail.com>
2024-03-18 00:59:36 -0700

    add type to auth.middleware.ts

--- src/middleware/authentication.middleware.ts
+++ src/middleware/authentication.middleware.ts
@@ -1,18 +1,20 @@
-export const ensureAuthenticated = (req, res, next) => {
+import express from "express";
+
+export const ensureAuthenticated = (req: express.Request, res: express.Response, next: express.NextFunction) => {
   if (req.isAuthenticated()) {
     return next();
   }
   res.redirect("/auth/login");
 };
 
-export const forwardAuthenticated = (req, res, next) => {
+export const forwardAuthenticated = (req: express.Request, res: express.Response, next: express.NextFunction) => {
   if (!req.isAuthenticated()) {
     return next();
   }
   res.redirect("/posts");
 };
 
-export const setCurrentUser = (req, res, next) => {
+export const setCurrentUser = (req: express.Request, res: express.Response, next: express.NextFunction) => {
   if (req.isAuthenticated()) {
     res.locals.currentUser = req.user;
   }
