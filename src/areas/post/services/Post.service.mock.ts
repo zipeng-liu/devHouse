aaf59b49afb1df42b40a47e045335145528e57b2
alesstongwen <alesstongwen@gmail.com>
2024-03-18 23:08:26 -0700

    attempt on doing post

--- src/areas/post/services/Post.service.mock.ts
+++ src/areas/post/services/Post.service.mock.ts
@@ -1,19 +1,21 @@
 import IPost from "../../../interfaces/post.interface";
 import IPostService from "./IPostService";
 
+const postsDB: IPost[] = [];
 // ⭐️ Feel free to change this class in any way you like. It is simply an example...
 export class MockPostService implements IPostService {
   addPost(post: IPost, username: string): void {
     // 🚀 Implement this yourself.
-    throw new Error("Method not implemented.");
+    postsDB.push(post);
   }
   getAllPosts(username: string): IPost[] {
     // 🚀 Implement this yourself.
+
     throw new Error("Method not implemented.");
   }
   findById(id: string): IPost {
     // 🚀 Implement this yourself.
-    throw new Error("Method not implemented.");
+    return postsDB.find((post) => post.postId === id);
   }
   addCommentToPost(message: { id: string; createdAt: string; userId: string; message: string }, postId: string): void {
     // 🚀 Implement this yourself.
