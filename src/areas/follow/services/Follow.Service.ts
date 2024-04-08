432ce722c6dea9736b42ed6d04ae8f0166d40483
denis <denis961111@gmail.com>
2024-04-08 02:25:15 -0700

    make sure user can't follow self

--- src/areas/follow/services/Follow.Service.ts
+++ src/areas/follow/services/Follow.Service.ts
@@ -5,6 +5,51 @@ import type { User } from "@prisma/client";
 export class FollowService implements ISettingService {
   readonly _db: DBClient = DBClient.getInstance();
 
+  // async getFollowedUsers(userId: string): Promise<User[]> {
+  //   const followedUserIds = (await this._db.prisma.follows.findMany({
+  //     where: {
+  //       followingId: userId,
+  //     },
+  //     select: {
+  //       followedById: true,
+  //     },
+  //   })).map((follow) => follow.followedById);
+
+  //   const followedUsers = await this._db.prisma.user.findMany({
+  //     where: {
+  //       id: {
+  //         in: followedUserIds,
+  //       },
+  //     },
+  //   });
+
+  //   return followedUsers;
+  // }
+
+  // async getUnfollowedUsers(userId: string): Promise<User[]> {
+  //   const followedUserIds = (await this._db.prisma.follows.findMany({
+  //     where: {
+  //       followingId: userId,
+  //     },
+  //     select: {
+  //       followedById: true,
+  //     },
+  //   })).map((follow) => follow.followedById);
+
+  //   const unfollowedUsers = await this._db.prisma.user.findMany({
+  //     where: {
+  //       NOT: {
+  //         id: {
+  //           in: followedUserIds,
+  //         },
+  //       },
+  //     },
+  //   });
+
+  //   return unfollowedUsers;
+  // }
+
+
   async getFollowedUsers(userId: string): Promise<User[]> {
     const followedUserIds = (await this._db.prisma.follows.findMany({
       where: {
@@ -14,18 +59,20 @@ export class FollowService implements ISettingService {
         followedById: true,
       },
     })).map((follow) => follow.followedById);
-
+  
+    const filteredFollowedUserIds = followedUserIds.filter(id => id !== userId);
+  
     const followedUsers = await this._db.prisma.user.findMany({
       where: {
         id: {
-          in: followedUserIds,
+          in: filteredFollowedUserIds,
         },
       },
     });
-
+  
     return followedUsers;
   }
-
+  
   async getUnfollowedUsers(userId: string): Promise<User[]> {
     const followedUserIds = (await this._db.prisma.follows.findMany({
       where: {
@@ -40,15 +87,16 @@ export class FollowService implements ISettingService {
       where: {
         NOT: {
           id: {
-            in: followedUserIds,
+            in: [userId, ...followedUserIds],
           },
         },
       },
     });
 
     return unfollowedUsers;
-  }
+}
 
+  
   async followUser(userId: string, followedUserId: string): Promise<void> {
     await this._db.prisma.follows.create({
       data: {
